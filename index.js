3f17059f9689e6fd7f80292593bb4b4bfc0305c9
Dmytro82 <dmutro8228@gmail.com>
2021-10-22 17:56:26 +0300

    11 color js

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startRefs = document.querySelector('button[data-action="start"]');
+const stopRefs = document.querySelector('button[data-action="stop"]');
+const bodyRefs = document.querySelector('body');
+let intervalId = null;
+
+function randomColors() {
+  const currentColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  return currentColor;
+}
+
+startRefs.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    bodyRefs.style.backgroundColor = randomColors();
+  }, 1000);
+  startRefs.disabled = true;
+});
+
+stopRefs.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startRefs.disabled = false;
+  bodyRefs.style.backgroundColor = '#fff';
+});
